df497d5aaad0107ecab9e93054a33a2c653b5bc3
AdrianPC <adrianchooo18@gmail.com>
2021-08-10 10:13:08 -0500

    edad valida

--- First/js/script.js
+++ First/js/script.js
@@ -2,10 +2,9 @@ var edad = prompt("Ingrese su edad");
 
 
 //VERIFICACION DE MAYORIA DE EDAD
-if (Number() = edad) {
   if (edad > 18) {
     document.write("Puede conducir");
-  }
-}
-else {
-  alert("Introduce un número válido")};
+  }else {
+    alert("Introduce un número válido")};
+
+
